90cc19ed06e094ffed6a080b24120e27e3b0d644
Lena <l024@tutanota.com>
2020-10-28 09:30:07 +0000

    Started working on projects cards

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import '../styles/styles.css'
+import RevealOnScroll from './modules/RevealOnScroll'
+
+
+new RevealOnScroll(document.querySelectorAll(".projects__item"), 75);
 
 if (module.hot) {
   module.hot.accept()
 }
-
-console.log("It's working");
