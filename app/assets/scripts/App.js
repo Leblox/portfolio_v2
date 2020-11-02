6ac025f1980c9ff457bb83c5c4c68e5f2ad4e075
Lena <l024@tutanota.com>
2020-11-02 17:58:23 +0000

    Content changes, project.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,10 @@ import '../styles/styles.css'
 import RevealOnScroll from './modules/reveal-on-scroll';
 import toggleClassWithButton from './modules/moreButton';
 
+if (module.hot) {
+  module.hot.accept()
+}
+
 // document.querySelector(".more-projects__title-button").addEventListener("click", () => console.log("Pressed button"));
 
 new RevealOnScroll();
